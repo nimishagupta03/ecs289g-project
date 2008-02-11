987ef56bdc5e8ce6f4272306b35e4a7a494cef3c
pfishero <pfishero@513bf4c5-6545-0410-b0ca-3f990637e5ca>
2008-02-11 00:01:00 +0000

    A recommender that allows you to combine other recommenders.

---
+++ trunk/MovieMiner/src/main/java/edu/ucdavis/cs/movieminer/taste/recommender/CompositeRecommender.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package edu.ucdavis.cs.movieminer.taste.recommender;
+
+import java.util.List;
+
+import com.planetj.taste.common.TasteException;
+import com.planetj.taste.impl.recommender.AbstractRecommender;
+import com.planetj.taste.model.DataModel;
+import com.planetj.taste.model.Item;
+import com.planetj.taste.recommender.RecommendedItem;
+import com.planetj.taste.recommender.Recommender;
+import com.planetj.taste.recommender.Rescorer;
+
+/**
+ * CompositeRecommender allows you to leverage more than one Recommender
+ * to estimate preferences.  The CompositeRecommender will take a weighted 
+ * average of the estimated preferences from each recommender to produce the 
+ * result, with the weights being specified via {@link #setWeights(double...)}.
+ *  
+ * @author pfishero
+ * @version $Id$
+ */
+public class CompositeRecommender extends AbstractRecommender {
+	private double[] weights;
+	private double weightsTotal;
+	private final Recommender[] recommenders;
+	
+	public CompositeRecommender(final DataModel dataModel, Recommender ... recommenders) {
+		super(dataModel);
+		this.recommenders = recommenders;
+	}
+	
+	public CompositeRecommender setWeights(double ... weights) {
+		this.weights = weights;
+		for (int i=0; i < weights.length; i++) {
+			weightsTotal += weights[i];
+		}
+		
+		return this;
+	}
+	
+	public double[] getWeights() {
+		return weights;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.planetj.taste.recommender.Recommender#estimatePreference(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public double estimatePreference(Object userId, Object itemId)
+			throws TasteException {
+		double preferrence = 0;
+		
+		for (int i=0; i < recommenders.length; i++) {
+			double prefEstimate = recommenders[i].estimatePreference(userId, itemId);
+			preferrence += prefEstimate * weights[i];
+		}
+		preferrence /= weightsTotal;
+		
+		return preferrence;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.planetj.taste.recommender.Recommender#recommend(java.lang.Object, int)
+	 */
+	@Override
+	public List<RecommendedItem> recommend(Object userId, int howMany)
+			throws TasteException {
+		throw new UnsupportedOperationException();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.planetj.taste.recommender.Recommender#recommend(java.lang.Object, int, com.planetj.taste.recommender.Rescorer)
+	 */
+	@Override
+	public List<RecommendedItem> recommend(Object userId, int howMany,
+			Rescorer<Item> rescorer) throws TasteException {
+		throw new UnsupportedOperationException();
+	}
+
+}
